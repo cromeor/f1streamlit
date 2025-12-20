65abf5a2d435f9eb7827c68e4452acb736e3bd06
Adil Alybaev <150300335+cromeor@users.noreply.github.com>
2025-12-19 20:02:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import streamlit as st
 import pages.season_leaderboard as season
-import pages.pit_stops as pits
+import pages.pit_stops as pit
 
-st.set_page_config(page_title="F1 Dashboard", layout="wide")
+st.set_page_config(
+    page_title="F1 Dashboard",
+    layout="wide"
+)
 
 st.title("🏎️ Formula 1 Dashboard")
 
 page = st.radio(
-    "Choose page",
+    "Navigate",
     ["Season Leaderboard", "Pit Stops Analysis"],
     horizontal=True
 )
@@ -15,4 +18,4 @@ page = st.radio(
 if page == "Season Leaderboard":
     season.render()
 else:
-    pits.render()
+    pit.render()
